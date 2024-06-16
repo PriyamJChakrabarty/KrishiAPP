23d3b35b8a62efeb358bbd54b9557bcc16922da8
Priyam Jyoti Chakrabarty <priyam1234work@gmail.com>
2024-06-16 08:45:52 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+
+
+app = FastAPI()
+
+
+MODEL = tf.keras.models.load_model("potatoes.h5")
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post('/')
+# async def index():
+#     return "Hello world"
+#
+#
+
+#
+#
+# @app.post("/predict")
+async def predict(
+        file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=8000)
+
